281e46edf207b3c50b156967c22e0fed0e8dd89d
Dora Ljubas <dl52294@fer.hr>
2023-05-15 17:59:06 +0200

    init commit

---
+++ src/main/java/infsus/dz3/dkbackend/DkBackendApplication.java
@@ -0,0 +1,13 @@
+package infsus.dz3.dkbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DkBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DkBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/infsus/dz3/dkbackend/DkBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package infsus.dz3.dkbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DkBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
