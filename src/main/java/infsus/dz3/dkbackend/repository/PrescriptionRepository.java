0995edcbd637f30d8323ca6c58da5bd61409baca
Dora Ljubas <dl52294@fer.hr>
2023-05-17 14:37:54 +0200

    @repo annotation

--- src/main/java/infsus/dz3/dkbackend/repository/PrescriptionRepository.java
+++ src/main/java/infsus/dz3/dkbackend/repository/PrescriptionRepository.java
@@ -1,13 +1,16 @@
 package infsus.dz3.dkbackend.repository;
 
 import infsus.dz3.dkbackend.model.Prescription;
+import lombok.AllArgsConstructor;
 import org.springframework.jdbc.core.BeanPropertyRowMapper;
 import org.springframework.jdbc.core.namedparam.MapSqlParameterSource;
 import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
 import org.springframework.jdbc.core.namedparam.SqlParameterSource;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
-
+@Repository
+@AllArgsConstructor
 public class PrescriptionRepository {
 
     NamedParameterJdbcTemplate namedParameterJdbcTemplate;
